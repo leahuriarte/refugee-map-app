42961df60233d949e2ba27134e9be3fcd3bc8ce5
leah uriarte <leahu257@gmail.com>
2025-05-01 17:02:15 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      crypto: resolve(__dirname, './crypto-polyfill.js')
+    }
+  },
+  build: {
+    sourcemap: false,
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
+  }
 })
