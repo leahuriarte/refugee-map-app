ad6ac2050cd13f9520eeefe6091150b27c6f4f4b
leah uriarte <leahu257@gmail.com>
2025-05-02 20:43:44 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,21 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      crypto: resolve(__dirname, './crypto-polyfill.js')
-    }
-  },
   build: {
     sourcemap: false,
     commonjsOptions: {
       transformMixedEsModules: true
+    }
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis'
+      }
     }
   }
 })
